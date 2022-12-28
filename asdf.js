fce50d70fbef5d05656c415077afc8cb4f842c5c
wlauddlqslek <wlaud070510@gmail.com>
2022-12-29 00:11:12 +0900

    Update asdf.js

--- asdf.js
+++ asdf.js
@@ -9,7 +9,7 @@ function updatejuje() {
 }
 
 function updatemokrok() {
-    if (words) {
+    if (words != []) {
         showjujejunche.value = jujejunche();
         showjujegim.value = jujegim();
     } else {

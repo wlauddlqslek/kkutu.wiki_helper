35dc1a4f3128b82c494a4ecabfce1b9ec61f8e0c
wlauddlqslek <wlaud070510@gmail.com>
2022-12-30 00:15:33 +0900

    Update asdf.js

--- asdf.js
+++ asdf.js
@@ -1,16 +1,18 @@
 window.onload = function () {
 
-    inputdocument = document.getElementById("inputdocument"); // 문서 원본 입력
-    inputwords = document.getElementById("inputwords"); // 단어 입력
-    inputjuje = document.getElementById("inputjuje"); // 주제 입력
-    showjujejunche = document.getElementById("showjujejunche"); // 주제 전체 단어 목록 보여주기
-    showjujegim = document.getElementById("showjujegim"); // 주제 긴 단어 목록 보여주기
-    copyjujejunche = document.getElementById("copyjujejunche"); // 주제 전체 단어 목록 복사 버튼
-    copyjujegim = document.getElementById("copyjujegim"); // 주제 긴 단어 목록 복사 버튼
+    const inputdocument = document.getElementById("inputdocument"); // 문서 원본 입력
+    const inputwords = document.getElementById("inputwords"); // 단어 입력
+    const inputjuje = document.getElementById("inputjuje"); // 주제 입력
+    const showjujejunche = document.getElementById("showjujejunche"); // 주제 전체 단어 목록 보여주기
+    const showjujegim = document.getElementById("showjujegim"); // 주제 긴 단어 목록 보여주기
+    const copyjujejunche = document.getElementById("copyjujejunche"); // 주제 전체 단어 목록 복사 버튼
+    const copyjujegim = document.getElementById("copyjujegim"); // 주제 긴 단어 목록 복사 버튼
 
     // words 중복 제거 & 가나다순 정렬
     function updatewords() {
-        words = [...new Set([...wordsorigin, ...wordsnew])].sort();
+        words = [...new Set([...wordsorigin, ...wordsnew])]
+        .filter(a => a !== '') // 비었을 시 거르기
+        .sort(); // 가나다순 정렬
     }
     // juje 가져오기
     function updatejuje() {
@@ -51,13 +53,11 @@ window.onload = function () {
     }
     // 주제 긴 단어 목록 만들기
     function jujegim() {
-        var a = words.filter(function (a) { // 9글자 이상 단어 가져오기
-            return a.length >= 9
-        }).sort(function (a, b) {
-            return b.length - a.length // 긴 순 정렬
-        }).map(function (a) {
-            return `|-\n| ${a.length} || [[${a}]]`
-        }).join("\n"); // 셀 안에 든 단어 형태로 만든 후 문자열로 변환
+        var a = words
+        .filter(a => a.length >= 9) // 9글자 이상 단어 가져오기
+        .sort((a, b) => b.length - a.length) // 긴 순 정렬
+        .map(a => `|-\n| ${a.length} || [[${a}]]`) // 셀 안에 든 단어 형태로 만들기
+        .join("\n"); // 문자열로 변환
 
         return `[[분류:${juje}]][[분류:긴 단어 목록]]\n{{상위 문서|${juje == '분홍꽃' ? '분홍꽃(주제)|분홍꽃' : juje}}}\n== 개요 ==\n[[${juje == '분홍꽃' ? '분홍꽃(주제)|분홍꽃' : juje}]] 주제의 긴 단어 목록이다.\n\n== 목록 ==\n{| class="wikitable sortable" style="text-align: center;"\n! width="50" | 길이 !! 단어\n${a}\n|}` // 문서 형태로 만들기
     }
@@ -69,22 +69,18 @@ window.onload = function () {
     juje = '';
 
     inputdocument.onkeyup = function () {
-        if (inputdocument.value.match(/(?<=\| \[\[).+(?=\]\])/g)) {
-            wordsdocument = inputdocument.value.match(/(?<=\|\s\[\[).+(?=\]\])/g).filter(function (a) {
-                return a !== '';
-            }); // 대괄호 안 문자열을 배열로 만들기
-        } else {
-            wordsdocument = [];
-        }
+        var a = inputdocument.value.match(/(?<=\| \[\[).+(?=\]\])/g);
+
+        wordsdocument = a ? a : []; // 대괄호 안 문자열이 있을 시 대괄호 안 문자열을 배열로 만들기
         wordsorigin = wordsdocument;
+
         updatewords();
         updatejuje();
         updatemokrok();
     };
     inputwords.onkeyup = function () {
-        wordsnew = inputwords.value.split("\n").filter(function (a) {
-            return a !== '';
-        }) // 문자열을 배열로 만들기
+        wordsnew = inputwords.value.split("\n") // 문자열을 배열로 만들기
+
         updatewords();
         updatemokrok();
     };
